f5beace290ff52e161d40c47a690143263e62a22
idolevin <idolevin@b5fd3887-524f-0410-bece-afd1f1f7cb07>
2008-06-15 19:42:53 +0000

    Strategy + MatrixHandler Weight, getPredictions Works at last

--- trunk/src/domain/controller/Strategy.java
+++ trunk/src/domain/controller/Strategy.java
@@ -1,5 +1,6 @@
 package domain.controller;
 
+import Exceptions.RateNotAtRangeException;
 import domain.Movie;
 import domain.User;
 import java.util.HashMap;
@@ -8,5 +9,5 @@ import java.util.Vector;
 public interface Strategy {
     public void set_users(HashMap<Integer, User> _users);
     public void set_movies(HashMap<Integer, Movie> _movies);
-    public Vector[] get10Recomendations(int userId);
+    public Vector[] getRecomendations(int userId) throws RateNotAtRangeException;
 }
