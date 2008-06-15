0aa07bf619ebdc011591c5daebe8af87c815f22b
idolevin <idolevin@b5fd3887-524f-0410-bece-afd1f1f7cb07>
2008-06-15 11:24:30 +0000

    informer tests

---
+++ trunk/tests/informer/XMLUtilityManagmentTest.java
@@ -0,0 +1,19 @@
+package informer;
+
+import static org.junit.Assert.*;
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class XMLUtilityManagmentTest extends TestCase{
+
+	@Test
+	public void testReadXMLFile() {
+		 assertTrue(1 == 1 );
+	}
+
+	@Test
+	public void testWriteXMLFile() {
+		assertTrue(1 == 1 );
+	}
+}
