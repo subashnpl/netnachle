990d8e97f2bf2cf33dd09d973e8b47a41c77b947
shaigi@gmail.com <shaigi@gmail.com@b5fd3887-524f-0410-bece-afd1f1f7cb07>
2008-07-25 14:54:09 +0000

    After seperate controller's and domain's packages (for tests also)

---
+++ trunk/tests/controller/ControllerTest.java
@@ -0,0 +1,196 @@
+package controller;
+
+import static org.junit.Assert.*;
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+import domain.Movie;
+import domain.User;
+import controller.Controller;
+import controller.Strategy;
+
+public class ControllerTest extends TestCase{
+
+	@Test
+	public void testController() {
+		
+	}
+
+	@Test
+	public void testGetMovieNames() {
+		
+	}
+
+	@Test
+	public void testGetUsersIds() {
+		
+	}
+
+	@Test
+	public void testGetUsersNamesById() {
+		
+	}
+
+	@Test
+	public void testGetMoviesIds() {
+		
+	}
+
+	@Test
+	public void testGetMoviesNamesById() {
+		
+	}
+
+	@Test
+	public void testGet_movieHandler() {
+		
+	}
+
+	@Test
+	public void testRemoveUserInt() {
+		
+	}
+
+	@Test
+	public void testRemoveMovieInt() {
+		
+	}
+
+	@Test
+	public void testGetMoviesToRate() {
+		
+	}
+
+	@Test
+	public void testSetRatesByUser() {
+		
+	}
+
+	@Test
+	public void testGetUsersNames() {
+
+	}
+
+	@Test
+	public void testSet_movieHandler() {
+
+	}
+
+	@Test
+	public void testGet_usersHandler() {
+
+	}
+
+	@Test
+	public void testSet_usersHandler() {
+
+	}
+
+	@Test
+	public void testGet_users() {
+
+	}
+
+	@Test
+	public void testSet_users() {
+
+	}
+
+	@Test
+	public void testGet_usersIn() {
+
+	}
+
+	@Test
+	public void testSet_usersIn() {
+
+	}
+
+	@Test
+	public void testGet_movies() {
+
+	}
+
+	@Test
+	public void testSet_movies() {
+
+	}
+
+	@Test
+	public void testAddUser() {
+
+	}
+
+	@Test
+	public void testGet_Strategy() {
+
+	}
+
+	@Test
+	public void testSet_Strategy() {
+
+	}
+
+	@Test
+	public void testRemoveUserUser() {
+		
+	}
+
+	@Test
+	public void testAddMovie() {
+		
+	}
+
+	@Test
+	public void testRemoveMovieMovie() {
+		
+	}
+
+	@Test
+	public void testUserSaw() {
+	
+	}
+
+	@Test
+	public void testShutDown() {
+		
+	}
+
+	@Test
+	public void testSignUp() {
+	
+	}
+
+	@Test
+	public void testLogin() {
+		
+	}
+
+	@Test
+	public void testLogout() {
+		
+	}
+
+	@Test
+	public void testRecieveRecommendations() {
+		
+	}
+
+	@Test
+	public void testGetCurrentUser() {
+	
+	}
+
+	@Test
+	public void testSetCurrentUser() {
+		
+	}
+
+	@Test
+	public void testMain() {
+		
+	}
+
+}
---
+++ trunk/tests/controller/LoggerTest.java
@@ -0,0 +1,30 @@
+package controller;
+
+import static org.junit.Assert.*;
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class LoggerTest extends TestCase{
+
+	@Test
+	public void testMakeSingleton() {
+		
+	}
+
+	@Test
+	public void testLog() {
+		
+	}
+
+	@Test
+	public void testExit() {
+		
+	}
+
+	@Test
+	public void testToString() {
+		
+	}
+
+}
